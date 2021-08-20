15219f1bcd61608b7e8f73e0389d28a353709f81
rosindubh <p.welsby2@ntlworld.com>
2021-08-20 10:31:48 +0100

    push file for Driss

---
+++ buttons.js
@@ -0,0 +1,13 @@
+// phil welsby 20 aug 2021
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+
+const name = "buttons";
+
+module.exports = {
+    add,
+    desc: "This is the buttons function page",
+    noOfFunctions: 1
+}
